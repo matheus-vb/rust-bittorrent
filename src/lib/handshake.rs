ddce19530b3dd108e9c0e4084a5b63dbe91ec380
Matheus Batista <matheus.vbatista04@gmail.com>
2024-10-26 17:14:40 -0300

    add handshake structure

---
+++ src/lib/handshake.rs
@@ -0,0 +1,22 @@
+// Using C repr so that, indirectly, can be treated as a array of bytes, so that it can be
+// cast as [u8; size_of::<Handshake>()]
+#[repr(C)]
+pub struct Handshake {
+    pub length: u8,
+    pub bittorrent: [u8; 19],
+    pub reserved: [u8; 8],
+    pub info_hash: [u8; 20],
+    pub peer_id: [u8; 20],
+}
+
+impl Handshake {
+    pub fn new(info_hash: [u8; 20], peer_id: [u8; 20]) -> Self {
+        Handshake {
+            length: 19,
+            bittorrent: *b"BitTorrent protocol",
+            reserved: [0; 8],
+            info_hash,
+            peer_id,
+        }
+    }
+}
